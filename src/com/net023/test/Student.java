21d51d0610274c672be8f273ca0fe109b8185702
net023 <chaitao1990617@hotmail.com>
2016-08-15 00:38:27 +0800

    sfdsdfsdfsdf

---
+++ src/com/net023/test/Student.java
@@ -0,0 +1,10 @@
+package com.net023.test;
+
+/**
+ * Created by Administrator on 2016/8/15.
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
